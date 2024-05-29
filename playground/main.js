9b1ec52495bfcf794f5befba1b9aaf5b2c903e24
charlieroberts <charlie@charlie-roberts.com>
2024-05-29 09:05:43 -0700

    change repeat operator in demo code

--- playground/main.js
+++ playground/main.js
@@ -134,7 +134,7 @@ const setupEditor = function() {
       sd,
       p,
       basicDark,
-      //editableCompartment.of(EditorView.editable.of(true)),
+      editableCompartment.of( EditorView.editable.of( true ) ),
       // only close ( and [
       sd.data.of({closeBrackets: {brackets: ['(', '[']}})
     ],
@@ -236,10 +236,10 @@ sphere'1.5
 cone'5
 
 // repeated boxes?
-box | 3
+box # 3
 
 // smaller boxes, more repeats
-box'.1 | .4
+box'.1 # .4
 
 // we can specify better render settings for large repeats
 // run these lines one by one. alternatively you can run the 
@@ -248,17 +248,17 @@ box'.1 | .4
 // the quickest way to run most examples in this tutorial...
 // just click in a block and hit alt+enter.
 render = repeat.med
-box'.1 | .4
+box'.1 # .4
 
 // a little fog makes things purdy
 // first, the amount of fog, then, the color
 // screamer will also remember your last used
 // render settings (repeat.med)
 fog = (.25,0,0,0)
-sphere '.1 | .4
+sphere '.1 # .4
 
 // play with your mouse
-sphere ' mousex | mousey * 4
+sphere ' mousex # mousey * 4
 
 // animate with time
 fog = (0,0,0,0)
@@ -294,29 +294,30 @@ julia( 5+sin(time/3) ) '2 : red :: stripes
 // texture boxes
 render = repeat.med
 fog = (.25,0,0,0)
-(box@(time*25,0,1,0)::rainbow'.2 | .75)
+(box@(time*25,0,1,0)::rainbow'.2 # .75)
 
 // hit alt+c, then use WASD and the arrow keys to explore
 // hit alt+c again to resume editing
 
 // subtract a repeat
 fog = (0,0,0,0)
-(box:red -- box:green'.1|.3)@(time*5,0,1,1)
+(box:red -- box:green'.1#.3)@(time*5,0,1,1)
 
 // smooth out the jaggies with post-processing
 post = ( antialias(3) )
-(box:red -- box:green'.1|.3)@(time*5,0,1,1)
+(box:red -- box:green'.1#.3)@(time*5,0,1,1)
 
 // fun with postprocessing and mouse
 post = ( edge, invert(1) )
-(box:red -- box:green'mousey/4|.2+mousex/3)'1.6@(time*15,0,1,1)
+(box:red -- box:green'mousey/4#.2+mousex/3)'1.6@(time*15,0,1,1)
 
 // use low and high audio analysis to drive fractal
 // the first time you use the low,med,or high variables
 // your browser will request permission to access your
 // audio input. shushing into your mic works well for 
 // this demo :)
-render=fractal.med
+post   = ()
+render = fractal.med
 mandalay( high*5, low/4, 2 )'.75@(time*5,0,0,1)::rainbow
 
 // for a more complete reference see
