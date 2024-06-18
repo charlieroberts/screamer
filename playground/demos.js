3c13ba8f273b3d4f4608c92669d969e5affd0938
charlieroberts <charlie@charlie-roberts.com>
2024-06-18 08:28:57 -0700

    lower resource requirements for most demos

--- playground/demos.js
+++ playground/demos.js
@@ -1,5 +1,5 @@
 const demos = [
-`render = repeat.med 
+`render = repeat.low 
 fog = (.5 0 0 0)
 camera = (0 0 time*.25)
 
@@ -16,10 +16,10 @@ post = ( antialias )
 fog = (1 0 0 0)
 camera = (0 0 .65)
 post = (bloom(.55 1.25))
-[torus((1,.05)):white 10 >xy i*.125 @i*10*sin(time/6) ||(.001)] '.05 @y time*10
+[torus((1,.05)):white 10 >xy i*.125 @i*10*sin(time/6) ||(.001)] '.035 @y time*10
 `,
 
-`render = voxel.med
+`render = voxel.low
 fog = (.2 0 .3 0)
 background = (0 .3 0)
 camera = (0 0 time*-.1)
@@ -51,29 +51,26 @@ mandalay( high*5, .25, 4 )'.75 @z time*5 ::rainbow(.3)
 
 
 `fog = (.05 1 0 0)
-post = ( antialias(2) )
+background = (.5 0 .25)
+post = ( antialias )
  
 head = sphere :magenta
-lear = sphere'.25 > .75:red
-rear = sphere'.25 >(-.75,.75,.75):red
-mouth = torus((.35,.05)) >(0,-.5,.55) @x 30 :green ++ torus((.35,.05)) >(0,-.6,.55) :green
+ears = sphere'.25 > .75:red |x
+mouth = torus((.35,.05)) >(0,-.5,.55) @x 30 :green
 nose = sphere'.1 > (0 0 1) :red >y -.1
-lpupil = sphere:black '.1 >(-.25 .15 .95)
-leye = lpupil ++ sphere:glue '.5 >(-.25 .15 .5)
-rpupil = sphere:black '.1 >(.25 .15 .95)
-reye = rpupil ++ sphere:glue '.5 >(.25 .15 .5)
-face = ((head ++++(.2 3) lear ++++(.2 3) rear) ++(.05) mouth) ++ nose ++ leye ++ reye
+ 
+pupil = sphere:black '.1 >(.25 .15 .95)
+eye  = pupil ++ sphere:glue '.5 >(.25 .15 .5)
+eyes = eye |x
+face = ((head ++++(.2 3) ears ) ++(.05) mouth) ++ nose ++ eyes
 face >x 1.85 >z .35 >y -1.25
  
-body = sphere'.9 >y -1.35 >x .25::dots(2)
-body = body ++ sphere '.8 >y -1.5 >x -1.::dots(6)
-body = body ++ sphere '.5 >(-2,  -1.85,.25) ::dots(8)
-body = body ++ sphere '.3 >(-2.5,-2,   .5)::dots(10)
-body = body +++ sphere '.15 >(-2.4, -2.15, 1.05) ::dots(5)
-face @y sin(time)*15
-(face +++(.075) body:magenta) >y 1.25 >z .5 ++ plane:red ++ plane((0 0 1)):red
+body = ((sphere'.9 #x 1.5):magenta *** capsule((-1.5,0,0) (1.5,0,0))'1.45)::dots(10):magenta >x -1.5 >y -1.7
+ 
+face @y sin(time)*20
+ 
+(face+++(.075) body) >y 1.25 >z .5 ++ plane:red
 `
-
 ]
 
 export { demos }
