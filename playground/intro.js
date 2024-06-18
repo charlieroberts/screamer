a0485f78f36cd18956e22a75a92196b4cfcdb64c
charlieroberts <charlie@charlie-roberts.com>
2024-06-18 08:29:27 -0700

    make intro html instead of js

--- playground/intro.js
+++ playground/intro.js
@@ -1,26 +1,27 @@
-const intro = `// welcome to screamer!
+const intro = `<p>Welcome to <i>screamer</i>, a language for strange art. A screamer is a sped-up circus march composed to <a href="https://en.wikipedia.org/wiki/Screamer_(march)" target=_blank>"to stir audiences into a frenzy"</a>; this language uses a technique called raymarching to render graphical weirdness.</p>
 
-// coding on this site targets desktops, but mobile users can
-// click through demos using the "Next Demo >>" button in 
-// the top right corner. for the adventurous, mobile users
-// can also evaluate code using the $ key (or normal key
-// combos if you have a physical keyboard connected).
+<p>Coding on this site targets desktops, but mobile users can
+click through demos using the "Next Demo >>" button in 
+the bottom right corner. Adventurous mobile users
+can also evaluate code using the $ key, or normal key
+combos if you have an external keyboard connected to your mobile device.</p>
 
-// key commands (replace "alt" key with "option" key in macOS)
-// - alt + l loads the next demo
-// - ctrl + enter executes a line
-// - alt + enter executes a block
-// - shift + enter executes a block and resets default config
-// - ctrl + . clears the scene
-// - alt + c enables WASD + arrow keys camera control
+<p>Key commands are as follows (replace "alt" key with "option"
+key in macOS):</p>
 
-// interactive reference:
-// https://charlieroberts.github.io/screamer-docs
-//
-// tutorial:
-// https://charlieroberts.github.io/screamer/playground/?tutorial
-//
-// have fun!
-`
+<ul>
+<li><pre>alt + l</pre> loads the next demo
+<li><pre>ctrl + enter</pre> executes a line
+<li><pre>alt + enter</pre> executes a block
+<li><pre>shift + enter</pre> executes a block and resets default config
+<li><pre>ctrl + .</pre> clears the scene
+<li><pre>alt + c</pre> enables WASD + arrow keys camera control
+</ul>
+
+<p>For more help, see:</p>
+<a href="https://charlieroberts.github.io/screamer-docs">Interactive reference</a><br>
+<a href="https://charlieroberts.github.io/screamer/playground/?tutorial" target=_blank>Tutorial</a><br>
+
+<p>Click off this panel to dismiss it. Have fun!</p>`
 
 export default intro 
